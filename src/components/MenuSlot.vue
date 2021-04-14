e7c7ae5f5269af89223520457d2bac46070be269
Yanika44 <Yanika686@gmail.com>
2021-04-14 16:48:17 +0700

    62130500020 create menu page

---
+++ src/components/MenuSlot.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button class="text-xs text-center bg-green-400 float-right px-1 relative top-5 mr-1" @click="addToCart">+</button>
+    <img :src="getImagePath" alt="" class="w-full h-16 bg-red-400" />
+    <div class="flex justify-between text-xs">
+        <p>{{food.name}}</p>
+        <p>{{food.price}}</p>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuSlot",
+  methods: {
+    addToCart: ()=>{
+      
+    }
+  },
+  computed:{
+    getImagePath(){
+      return require("@/assets/" + this.food.image);
+    }
+  },
+  mounted() {
+  },
+  props: {
+    food: Object,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Menu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="grid grid-cols-5 gap-3 mx-12">
+      <menu-slot v-for="food in foods" :key="food.id" :food="food"></menu-slot>
+  </div>
+</template>
+
+<script>
+import MenuSlot from "../components/MenuSlot.vue";
+import axios from "axios";
+export default {
+    neme: "Menu",
+    data() {
+        return {
+            foods: null
+        }
+    },
+    mounted(){
+        axios.get("http://localhost:3000/menu").then((result)=>{
+            this.foods = result.data;
+        })
+    },
+    components: {
+        MenuSlot
+    }
+}
+</script>
+
+<style>
+
+</style>
